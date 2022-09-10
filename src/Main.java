bdef1f05b89a4c0945ef4015e76fe3082b38a4ec
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-10 18:58:35 +0300

    hw4pt2ex6

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         int deposit = 15_000;
         int depositPercent = 7;
-        for (int i = 1; deposit <= 12_000_000; i++) {
+        for (int i = 1; i <= 12*9; i++) {
             deposit = deposit + deposit * depositPercent / 100;
             if (i % 6 == 0) {
                 System.out.println("Месяц " + i + " , сумма накоплений составила " + deposit + " рублей");
