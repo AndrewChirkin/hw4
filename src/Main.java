e5fcb47140a6a402008fa1a3404335828882ecda
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-10 18:53:37 +0300

    hw4pt2ex5

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,12 @@ public class Main {
 
         int deposit = 15_000;
         int depositPercent = 7;
-        for(int i = 1;deposit <= 12_000_000; i++){
-            deposit = deposit + deposit*depositPercent/100;
-            System.out.println("Месяц " + i + " , сумма накоплений составила " + deposit + " рублей");
-        }
+        for (int i = 1; deposit <= 12_000_000; i++) {
+            deposit = deposit + deposit * depositPercent / 100;
+            if (i % 6 == 0) {
+                System.out.println("Месяц " + i + " , сумма накоплений составила " + deposit + " рублей");
             }
-
         }
+
+    }
+    }
