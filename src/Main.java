707dc2f0c97eb5360d370d20a5fd69aacf4e0447
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-10 17:00:10 +0300

    hw4pt1ex8

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,49 @@
 public class Main {
     public static void main(String[] args) {
+int salary = 29000;
+int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + salary;
+            switch (i){
+                case 1:
+                    System.out.println("Месяц январь, сумма накоплений равна " + total +" рублей");
+                    break;
+                case 2:
+                    System.out.println("Месяц февраль, сумма накоплений равна " + total +" рублей");
+                    break;
+                case 3:
+                    System.out.println("Месяц март, сумма накоплений равна " + total +" рублей");
+                    break;
+                case 4:
+                    System.out.println("Месяц апрель, сумма накоплений равна " + total +" рублей");
+                    break;
+                case 5:
+                    System.out.println("Месяц май, сумма накоплений равна " + total +" рублей");
+                    break;
+                case 6:
+                    System.out.println("Месяц июнь, сумма накоплений равна " + total +" рублей");
+                    break;
+                case 7:
+                    System.out.println("Месяц июль, сумма накоплений равна " + total +" рублей");
+                    break;
+                case 8:
+                    System.out.println("Месяц август, сумма накоплений равна " + total +" рублей");
+                    break;
+                case 9:
+                    System.out.println("Месяц сентябрь, сумма накоплений равна " + total +" рублей");
+                    break;
+                case 10:
+                    System.out.println("Месяц октябрь, сумма накоплений равна " + total +" рублей");
+                    break;
+                case 11:
+                    System.out.println("Месяц ноябрь, сумма накоплений равна " + total +" рублей");
+                    break;
+                case 12:
+                    System.out.println("Месяц декабрь, сумма накоплений равна " + total +" рублей");
+                    break;
 
-        for (int i = 1; i <= 512; i = i * 2) {
-            System.out.println(i);
+            }
+
         }
     }
 }
