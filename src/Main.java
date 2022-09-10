51a30ace59e554c7d199c24c17cb902018bfaa92
AndreyChirkin <106620486+AndrewChirkin@users.noreply.github.com>
2022-09-10 18:51:31 +0300

    hw4pt2ex4

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-    int population = 12_000_000;
-        double birthRate = (double) 17/1000;
-        double deathRate = (double) 8/1000;
-    for(int i = 1; i <= 10; i++){
-        population = (int) (population + (population * birthRate) - (population * deathRate));
-        System.out.println( "Год " + i + " , численность населения составляет " + population + " человек");
-    }
+
+        int deposit = 15_000;
+        int depositPercent = 7;
+        for(int i = 1;deposit <= 12_000_000; i++){
+            deposit = deposit + deposit*depositPercent/100;
+            System.out.println("Месяц " + i + " , сумма накоплений составила " + deposit + " рублей");
+        }
             }
 
         }
